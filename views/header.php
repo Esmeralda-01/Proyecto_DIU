6ab943e488b46f1e1da2dd7c6b1a43a7b95a6ac8
allenor1090 <albertolleras1990@gmail.com>
2022-05-26 14:16:18 -0500

    cambio de estilos dropdowns

--- views/header.php
+++ views/header.php
@@ -18,7 +18,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse " id="navbarNavDropdown">
       <ul class="navbar-nav">        
         <li class="nav-item">
           <a class="nav-link" href="<?php echo VIEWS_PATH; ?>user/user_medicamentos.php">Medicamentos</a>
@@ -28,8 +28,8 @@
             Citas
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="<?php echo VIEWS_PATH; ?>user/user_citas_consulta.php">Consultar citas</a></li>
-            <li><a class="dropdown-item" href="<?php echo VIEWS_PATH; ?>user/user_citas_agregar.php">Agendar cita</a></li>
+            <li><a class="dropdown-item nombre-empresa" href="<?php echo VIEWS_PATH; ?>user/user_citas_consulta.php">Consultar citas</a></li>
+            <li><a class="dropdown-item nombre-empresa" href="<?php echo VIEWS_PATH; ?>user/user_citas_agregar.php">Agendar cita</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
